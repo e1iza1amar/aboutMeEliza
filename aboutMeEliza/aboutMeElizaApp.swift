96d889d91ec37761c4ec76275dfa68b9e5e5f363
Eliza Lamar <elizalamar7@gmial.com>
2023-06-23 15:00:13 -0400

    Initial Commit

---
+++ aboutMeEliza/aboutMeElizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeElizaApp.swift
+//  aboutMeEliza
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeElizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
